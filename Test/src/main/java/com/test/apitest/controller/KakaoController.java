66934fe34110df0f7e6dc765921d70996b3fc51d
minhee.son <44446425+Miniminis@users.noreply.github.com>
2019-09-06 00:16:04 +0900

    kakao pay Api test01 결제 승인 처리

---
+++ Test/src/main/java/com/test/apitest/controller/KakaoController.java
@@ -0,0 +1,39 @@
+package com.test.apitest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.test.apitest.service.KakaoPay;
+
+@Controller("/apitest")
+public class KakaoController {
+	
+	@Autowired
+    private KakaoPay kakaopay;
+    
+    
+    @GetMapping("/kakaoPay")
+    public void kakaoPayGet() {
+        
+    }
+    
+    @PostMapping("/kakaoPay")
+    public String kakaoPay() {
+        System.out.println("kakaoPay post............................................");
+        
+        return "redirect:" + kakaopay.kakaoPayReady();
+ 
+    }
+    
+    @GetMapping("/kakaoPaySuccess")
+    public void kakaoPaySuccess(@RequestParam("pg_token") String pg_token, Model model) {
+        System.out.println("kakaoPaySuccess get............................................");
+        System.out.println("kakaoPaySuccess pg_token : " + pg_token);
+        
+    }
+
+}
