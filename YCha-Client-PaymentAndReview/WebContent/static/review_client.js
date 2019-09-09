86b7937feac709db24b34c361f857a16a8f4de10
minhee.son <44446425+Miniminis@users.noreply.github.com>
2019-09-09 17:42:33 +0900

    [YCha-Payment ver 0.8] 별점 구현 완료

--- YCha-Client-PaymentAndReview/WebContent/static/review_client.js
+++
@@ -1,7 +0,0 @@
-/*별점 템플릿*/
-
-// initialize with defaults
-$("#input-id").rating();
-
-// with plugin options (do not attach the CSS class "rating" to your input if using this approach)
-$("#input-id").rating({'size':'lg'});
